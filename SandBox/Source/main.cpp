d182401c38acca65f4216fc26d54b36919579884
Dezlow <dezlow@bk.ru>
2021-09-05 20:25:48 +0700

    fix entry point

--- SandBox/Source/main.cpp
+++ SandBox/Source/main.cpp
@@ -7,21 +7,22 @@
 
 int main()
 {
+	SandBoxApp* app = new SandBoxApp;
 	try
 	{
-		SandBoxApp* app = new SandBoxApp;
-
 		app->Run();
-
-		delete app;
 	}
 	catch (const std::exception& ex)
 	{
 		std::cerr << ex.what() << '\n';
 		spdlog::get("log")->error(ex.what());
 
+		delete app;
+
 		return EXIT_FAILURE;
 	}
+
+	delete app;
 
 	return EXIT_SUCCESS;
 }
